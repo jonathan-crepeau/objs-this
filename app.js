edb6b7a9f33d1837e691511c53ebf81034b2811d
jonathan-crepeau <jon.crepeau@gmail.com>
2022-02-18 13:52:52 -0800

    Object review completed

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
+
+
+// SECTION Object Review:
+
+// 1. Print the following:
+
+    // 'hooteroll'
+    // 'Bobby weir'
+    // 'writing poems'
+    // 'banjo'
+
+// 2. Edit the following properties:
+
+    // change the alive property to true using dot notation with the object
+    // change the quote property to 'Everybody just be nice to each other' using bracket notation
+    // Add a method
+
+    // add a method to the garcia object named speak that logs out "Heyy, Thats a good song!"
+    // add a method to the garcia object named letsPlay that logs out "La daaa da da, La daa, da, da"
+
+  // const garcia = {
+  //   albums: [
+  //     "shady grove",
+  //     "run for the roses",
+  //     "hooteroll",
+  //     "Garcia",
+  //     "Reflections",
+  //   ],
+  //   instruments: [
+  //     "Vocals",
+  //     "pedal steel",
+  //     "electric guitar",
+  //     "acoustic guitar",
+  //     "banjo",
+  //   ],
+  //   alive: false,
+  //   quote: "Im shopping around for something to do that no one will like",
+  //   friends: [
+  //     {
+  //       name: "Bobby weir",
+  //       hobby: "guitar",
+  //     },
+  //     {
+  //       name: "Bob Dylan",
+  //       hobby: "writing songs",
+  //     },
+  //     {
+  //       name: "Allen Ginsberg",
+  //       hobby: "writing poems",
+  //     },
+  //     {
+  //       name: "Phil Lesh",
+  //       hobby: "Playing dope bass lines",
+  //     },
+  //   ],
+  // };
+
+
+// SECTION this Keyword
+
