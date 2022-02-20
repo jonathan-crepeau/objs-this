9619ed22b0403d42d4c3e548cfe3d98fddac673b
jonathan-crepeau <jon.crepeau@gmail.com>
2022-02-20 14:48:35 -0800

    Interval exercises completed

--- app.js
+++ app.js
@@ -59,3 +59,92 @@
 
 // SECTION this Keyword
 
+// const foo = {
+//   value: 12,
+//   getValue() {
+//     return this.value;
+//   },
+// };
+
+// 1. console.log 'this' inside the getValue function:
+
+// const foo = {
+//   value: 12,
+//   getValue() {
+//     console.log('Define ' + this);
+//     return this.value;
+//   },
+// };
+
+// 2. Create an object called person that has a property called name and a method called getName that when called returns the value of the name property.
+
+const person = {
+  name: 'Jonathan',
+  getName() {
+    return this.name;
+  },
+}
+
+
+// SECTION More 'this'
+
+// In JS, everything belongs to an object.
+// We use 'this' to tell us what object we are currently in.
+// This is called CONTEXT, not to be confused with scope.
+
+const taco = () => {
+  console.log(this, ' this is with arrow functions!');
+}
+
+// this is referring to the Window object.
+
+
+// SECTION - Let's Play With Intervals
+
+// const game = {
+//   start() {
+//     const gameTimer = setInterval( () => {
+//       console.log('hi', this)
+//     }, 1000);
+//   }
+// }
+
+// 1. Add a property to the 'game' object called timer and set the value equal to 10.
+
+// const game = {
+//   timer: 10,
+//   start() {
+//     const gameTimer = setInterval( () => {
+//       console.log('hi', this)
+//     }, 1000);
+//   }
+// }
+
+// 2. edit the start method to decrease the value of 'time' every second by the value of 1.
+
+// const game = {
+//   timer: 10,
+//   start() {
+//     const gameTimer = setInterval( () => {
+//       console.log('hi', this);
+//       game.timer -= 1;
+//     }, 1000);
+//   }
+// }
+
+// 3. Add a conditional inside of the 'setInterval' that checks to see if teh value has reached 0. If yes, console log 'game over' and clear the interval:
+
+const game = {
+  timer: 3,
+  start() {
+    const gameTimer = setInterval(() => {
+      if (game.timer === 0) {
+        console.log('game over');
+        clearInterval(gameTimer);
+      } else {
+        console.log('hi', this);
+        game.timer -= 1;
+      }
+    }, 1000);
+  },
+};
